c8523740cfe7bc63cee42624b3e8165663fa77a5
Devin Rayfield <devinrayfield@gmail.com>
2024-04-28 03:42:24 -0500

    Added Database folder

---
+++ Database/TestTables.sql
@@ -0,0 +1,10 @@
+USE RecipeRedux;
+GO
+
+SELECT * FROM Ingredient ORDER BY IngredientID ASC;
+
+SELECT * FROM Category ORDER BY CategoryID ASC;
+
+SELECT * FROM IngredientCategory;
+
+SELECT * FROM Users;
