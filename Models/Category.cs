87a879662c5931516cb62d4ac1dc2de3447d770f
Devin Rayfield <devinrayfield@gmail.com>
2024-07-04 19:56:20 -0500

    Delete Models directory

--- Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RecipeRedux.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        public ICollection<IngredientCategory> IngredientCategories { get; set; }
-    }
-}
--- Models/Ingredient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RecipeRedux.Models
-{
-    public class Ingredient
-    {
-        public int IngredientId { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<IngredientCategory> IngredientCategories { get; set; }
-    }
-}
